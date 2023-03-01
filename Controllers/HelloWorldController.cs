a44e8acf71679f198b8486e97bde87013ed2bda9
Campidellis <vicampidelli@hotmail.com>
2023-02-28 22:24:49 -0300

    first commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace BSIStore.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //  /HelloWorld
+
+        public string index()
+        {
+             return "Essa é uma ação principal...";
+        }
+    // /HelloWorld/Welcome
+    /*public string Welcome()
+    {
+        return "Ação do método Welcome";
+    }*/
+    public string Welcome(string nome, int ID=1)
+    {
+        return HtmlEncoder.Default.Encode($"Nome: {nome}, ID: {ID}");
+
+    }
+}
+
+}
